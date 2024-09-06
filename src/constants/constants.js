b733d09a5a1085532a0a8b1150d3844dcc93b6eb
chitz <54183293+shubham6350@users.noreply.github.com>
2024-09-06 20:24:20 +0530

    initial commit

---
+++ src/constants/constants.js
@@ -0,0 +1,50 @@
+export const projects = [
+  {
+    title: "Geek Chat",
+    description:
+      "Using CometChat Sdks, Firebase, React Native, and Redux, featuring real-time messaging, audio/video calling, and status updates. The app provides a seamless user experience with efficient state management and robust backend services.",
+    image: "/images/CometChat-Main-Screen.png",
+    tags: ["React Native", "Redux", "CometChat Sdk", "Firebase"],
+    source: "https://www.youtube.com/watch?v=VmIX65mLURc&t=30s",
+    visit: "https://google.com",
+    id: 0,
+  },
+  {
+    title: "Restaurant Inventory",
+    description:
+      "worked with a US client to develop a mobile app for inventory management for one of the largest restaurant chains in the US. The app was built using React Native Expo and React Query (TanStack Query) to ensure efficient data handling and seamless performance.",
+    image: "/images/kitchecn Inventory.png",
+    tags: ["React Native", "Expo", "react query"],
+    source: "",
+    visit: "",
+    id: 1,
+  },
+  {
+    title: "AI Bot",
+    description:
+      "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
+    image: "/images/3.jpg",
+    tags: ["React", "WebRTC"],
+    source: "https://google.com",
+    visit: "https://google.com",
+    id: 2,
+  },
+  {
+    title: "Liviit Mobile App",
+    description:
+      "worked a social media mobile app for an Australian client, which is live and stable. I worked on optimizing performance and stability while also designing and implementing key features like user profiles, content sharing, real-time updates, and interactive user feeds.",
+    image: "/images/liviit_app.jpg",
+    tags: ["React Native", "CometChat SDK", "Redux", "Firebase"],
+    source: "https://www.liviit.com/download",
+    visit: "https://google.com",
+    id: 3,
+  },
+];
+
+export const TimeLineData = [
+  { year: 2022, text: "Started my journey(worked as intern from june to oct)" },
+  { year: 2023, text: "Worked as full time Software engineer-I" },
+  { year: 2024, text: "working as Software engineer-II " },
+  // { year: 2024, text: "Shared my projects with the world" },
+  // { year: 2021, text: "Started my own platform" },
+];
